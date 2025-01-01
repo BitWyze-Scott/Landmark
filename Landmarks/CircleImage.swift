5c2dbdf2880e4919460bb2b929d0870543839da6
scottrichards <scott.srichards@gmail.com>
2025-01-01 16:52:48 +0100

    first commit

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Scott Richards on 12/31/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
