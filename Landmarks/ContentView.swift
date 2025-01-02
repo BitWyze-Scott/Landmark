4c9232b288c83adebe8a5d6f824ff2eb0c6a3326
scottrichards <scott.srichards@gmail.com>
2025-01-02 10:39:06 +0100

    Landmark First Tutorial

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,12 +10,33 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(.blue)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+
+                Text("Turtle Rock is a distinctive rock formation located in Joshua Tree National Park, named for its resemblance to the shape of a turtle. It is a popular spot for hiking, climbing, and photography, offering unique desert scenery and panoramic views of the surrounding landscape. The area is also known for its accessibility, making it a favorite among visitors seeking an iconic Joshua Tree experience.")
+            }
         }
         .padding()
+        
+        Spacer()
     }
 }
 
